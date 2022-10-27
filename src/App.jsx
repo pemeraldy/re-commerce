7f87f74ba9061b0027644dcc1e719cb50e0f10fb
Peter Olu <peter.o.olu@gmail.com>
2022-10-27 14:46:05 +0100

    skeletal filter sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Layout from "./components/Layout"
 import Navigation from "./components/Navigation"
+import Filters from "./components/Filters"
+
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <Navigation />
+        <div className="mt-10">
+          <Filters/>
+        </div>
       </Layout>
     </div>
   )
---
+++ src/components/Filters.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+function Filters() {
+  return (
+    <div className='flex justify-between items-center font-tenor'>
+      <h3 className='uppercase'>4500 Apparel</h3>
+
+        <form >
+          <div className='flex space-x-2'>
+            <select className='bg-gray-100 w-[72px] rounded-2xl' name="type" id="">
+              <option className='font-tenor' value="latest">
+                Latest
+              </option>
+              <option value="exp">Expensive</option>
+            </select>
+
+            <button type='button' className=' flex justify-center items-center w-[36px] h-[36px] rounded-full bg-gray-100'>
+              <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g opacity="0.5">
+                <path d="M10.1709 14.2729H16.6473" stroke="#14142B"/>
+                <path d="M10.1902 5.93958H16.6666" stroke="#14142B"/>
+                <rect x="2.17761" y="3.06879" width="5.66667" height="5.66667" stroke="#14142A"/>
+                <rect x="2.17761" y="11.4515" width="5.66667" height="5.66667" stroke="#14142A"/>
+                </g>
+                </svg>
+            </button>
+            <button type='button' className=' flex justify-center items-center w-[36px] h-[36px] rounded-full bg-gray-100'>
+              <svg width="16" height="8" viewBox="0 0 16 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M0.5 0.356232V1.18957H15.5V0.356232H0.5ZM6.33333 7.85623H9.66667V7.0229H6.33333V7.85623ZM13 4.5229H3V3.68956H13V4.5229Z" fill="#DD8560"/>
+              </svg>
+            </button>
+          </div>
+        </form>
+    
+    </div>
+  )
+}
+
+export default Filters
