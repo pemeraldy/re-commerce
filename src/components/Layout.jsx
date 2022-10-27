0990e7e5853241b8acd25bdb7151beba7b6223f4
Peter Olu <peter.o.olu@gmail.com>
2022-10-27 14:12:19 +0100

    import assets

---
+++ src/components/Layout.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Layout({children}) {
+  return (
+    <div className='max-w-[1260px] mx-auto bg-white py-[22px] px-2'>{children}</div>
+  )
+}
+
+export default Layout
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import Logo from '../assets/logo.svg'
+import Menu from '../assets/menuicon.svg'
+import SearchIcon from '../assets/search-icon.svg'
+import CartIcon from '../assets/cart.svg'
+
+function Navigation() {
+  return (
+     <div className="flex font-tenor justify-between">
+        <button>
+          <img src={Menu} alt="menu icon" />
+        </button>
+        
+        <a className=' cursor-pointer' heref="/">
+          <img src={Logo} alt="yopeeStore" />
+        </a>
+
+        <div className='flex items-center space-x-4 md:space-x-9'>
+          <button className='pt-1'>
+            <img src={SearchIcon} alt="" />           
+          </button>
+          <button>            
+            <img src={CartIcon} alt="" />
+          </button>
+        </div>
+     </div>
+  )
+}
+
+export default Navigation
